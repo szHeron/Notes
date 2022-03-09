ccce5cf1cf1641a6b327b487dd8233e54f4451ba
szheron <heronbr7@gmail.com>
2022-03-09 15:23:02 -0300

    added notification alert

--- src/components/RenderNotes/index.js
+++ src/components/RenderNotes/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { TouchableOpacity, Text} from 'react-native'
+import { View, TouchableOpacity, Text} from 'react-native'
+import { Feather } from '@expo/vector-icons';
 import Style from './style'
 
 export default function renderNote({item, navigation}){
     return(
         <TouchableOpacity style={Style.noteArea} onPress={()=>navigation.navigate('Notes', {note: item, search: true})}>
-            <Text style={Style.txtNoteTitle} numberOfLines={2}>{item.title}</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={Style.txtNoteTitle} numberOfLines={2}>{item.title}</Text>
+                {item.notificationId !== null && <Feather name="bell" size={12} color="gold"/>}
+            </View>
             <Text style={Style.txtNote} numberOfLines={6}>{item.note}</Text>
         </TouchableOpacity>
     )
