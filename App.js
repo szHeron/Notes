e06101110408dc10dedfa2655bdc04c6b209383e
Heron <84138510+szHeron@users.noreply.github.com>
2022-05-01 09:54:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import 'react-native-gesture-handler';
-import Route from './src/routes/route'
+import Route from './src/routes/route';
 
 export default function App(){
   return (
     <NavigationContainer>
       <Route/>
-      <StatusBar style='auto' />
+      <StatusBar style='auto'/>
     </NavigationContainer>
   );
 }
